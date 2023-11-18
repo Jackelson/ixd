f70e9f8b8a3a6998a63c2e3419b7d78f55b4b327
jackelson <95007281+Jackelson@users.noreply.github.com>
2023-11-18 11:20:13 +0800

    fix: 修改登录方式

--- src/router/intRouter.js
+++ src/router/intRouter.js
@@ -1,47 +1,97 @@
 /*
- * @Description: 
+ * @Description:
  * @Version: 2.0
  * @Autor: hjw
  * @Date: 2023-05-05 20:17:30
  * @LastEditors: hjw
  * @LastEditTime: 2023-06-17 21:29:12
  */
-import router from '@/router';
-var whiteList = ['/login', '/auth-redirect', '/config/control/index'] // no redirect whitelist
-router.beforeEach((to, from, next) => {
-	const hasToken = localStorage.getItem('createById')
-	if (hasToken) {
-		if (to.path === '/login') {
-			next()
-			// NProgress.done()
-		} else {
-			const hasRoles = 'this.$store.getters.getUserDetail'
-			if (hasRoles) {
-				next()
-			} else {
-				next(`/login`)
-				// 	try {
-				//   const role = await store.dispatch('user/getInfo')
-				//   const accessRoutes = await store.dispatch('permission/generateRoutes', role)
-				//   router.addRoutes(accessRoutes)
-				//   next({ ...to,
-				//     replace: true
-				//   })
-				// } catch (error) {
-				//   await store.dispatch('user/resetToken')
-				//   Message.error(error || 'Has Error')
-				//   next(`/login`)
-				//   NProgress.done()
-				// }
-			}
-		}
-	} else {
-		/* has no token*/
-		if (whiteList.indexOf(to.path) !== -1) {
-			next()
-		} else {
-			next(`/login`)
-			// NProgress.done()
-		}
-	}
+import router from "@/router";
+// var whiteList = ["/login", "/auth-redirect", "/config/control/index"]; // no redirect whitelist
+import { upLine } from "@/api/user";
+function getUrlParams(url) {
+  // 通过 ? 分割获取后面的参数字符串
+  let urlStr = url.split("?")[1];
+  // 创建空对象存储参数
+  let obj = {};
+  // 再通过 & 将每一个参数单独分割出来
+  let paramsArr = urlStr.split("&");
+  for (let i = 0, len = paramsArr.length; i < len; i++) {
+    // 再通过 = 将每一个参数分割为 key:value 的形式
+    let arr = paramsArr[i].split("=");
+    obj[arr[0]] = arr[1];
+  }
+  return obj;
+}
+
+router.beforeEach(async (to, from, next) => {
+  if (to.path === "/") {
+    const hasToken = localStorage.getItem("createById");
+    if (hasToken) {
+      next();
+    } else {
+      const currentUrl = window.location.href;
+      console.log(currentUrl);
+      const params = getUrlParams(currentUrl);
+      const res = await upLine(params);
+      if (res.code == 200) {
+        localStorage.setItem("createById", res.data.sysUser.userId);
+        localStorage.setItem("createByRole", res.data.sysUser.roles[0].roleId);
+        localStorage.setItem("updateBy", res.data.sysUser.dept.updateBy);
+        localStorage.setItem("remark", res.data.sysUser.dept.remark);
+        localStorage.setItem("remark", res.data.sysUser.dept.remark);
+        localStorage.setItem("menus", res.data.permissions);
+        localStorage.setItem("userName", res.data.sysUser.userName);
+        localStorage.setItem("token", res.data.toekn);
+        this.$store.commit("setUserDetail", res.data);
+      }
+    }
+  } else {
+    next();
+  }
+  //   const hasToken = localStorage.getItem("createById");
+  //   if (hasToken) {
+  //     const hasRoles = "this.$store.getters.getUserDetail";
+  //     if (hasRoles) {
+  //       next();
+  //     }
+  //   }
 });
+
+// router.beforeEach((to, from, next) => {
+// 	const hasToken = localStorage.getItem('createById')
+// 	if (hasToken) {
+// 		if (to.path === '/login') {
+// 			next()
+// 			// NProgress.done()
+// 		} else {
+// 			const hasRoles = 'this.$store.getters.getUserDetail'
+// 			if (hasRoles) {
+// 				next()
+// 			} else {
+// 				next(`/login`)
+// 				// 	try {
+// 				//   const role = await store.dispatch('user/getInfo')
+// 				//   const accessRoutes = await store.dispatch('permission/generateRoutes', role)
+// 				//   router.addRoutes(accessRoutes)
+// 				//   next({ ...to,
+// 				//     replace: true
+// 				//   })
+// 				// } catch (error) {
+// 				//   await store.dispatch('user/resetToken')
+// 				//   Message.error(error || 'Has Error')
+// 				//   next(`/login`)
+// 				//   NProgress.done()
+// 				// }
+// 			}
+// 		}
+// 	} else {
+// 		/* has no token*/
+// 		if (whiteList.indexOf(to.path) !== -1) {
+// 			next()
+// 		} else {
+// 			next(`/login`)
+// 			// NProgress.done()
+// 		}
+// 	}
+// });
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,144 +1,153 @@
 /*
- * @Description: 
+ * @Description:
  * @Version: 2.0
  * @Autor: hjw
  * @Date: 2023-05-05 20:17:30
  * @LastEditors: hjw
  * @LastEditTime: 2023-08-01 15:30:41
  */
-import axios from 'axios';
+import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
-import router from "@/router/index"
-import {ElMessage} from "element-plus"
+import router from "@/router/index";
+import { ElMessage } from "element-plus";
 // axios 配置
 axios.defaults.timeout = 300000;
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
-axios.defaults.baseURL = 'ixdpc';
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded;charset=UTF-8";
+axios.defaults.baseURL = "ixdpc";
 axios.defaults.withCredentials = true;
-axios.defaults.headers = { 'X-Requested-With': 'XMLHttpRequest' }; //请求头
+axios.defaults.headers = { "X-Requested-With": "XMLHttpRequest" }; //请求头
 // POST传参序列化
 axios.interceptors.request.use(
-    config => {
-        if (config.url != '/SysUser/login') {
-            let uuid = uuidv4();
-            uuid = uuid.replace(/-/g, "")
-            config.headers['token'] = localStorage.getItem('token') + '|' + uuid;
-        }
-        config.headers.Authorization = localStorage.getItem('authorization');
-        return config;
-    },
-    error => {
-        console.log(error,'error')
-        return Promise.reject();
+  (config) => {
+    if (config.url != "/SysUser/login") {
+      let uuid = uuidv4();
+      uuid = uuid.replace(/-/g, "");
+      config.headers["token"] = localStorage.getItem("token") + "|" + uuid;
     }
+    config.headers.Authorization = localStorage.getItem("authorization");
+    return config;
+  },
+  (error) => {
+    console.log(error, "error");
+    return Promise.reject();
+  }
 );
 
 // 返回状态判断
 axios.interceptors.response.use(
-    response => {   
-        if (response.status === 200) {
-            return response;
-        } else {
-            Promise.reject();
-        }
-    },
-    error => {
-        if(error.response.status == 401){
-            ElMessage.warning("登录过期，请重新登录")
-            router.push('/login')
-        }else {
-            ElMessage.warning("请求错误，请联系管理员")
-        }
-        return Promise.reject();
+  (response) => {
+    console.log(response);
+    if (response.status === 200) {
+      return response;
+    } else {
+      Promise.reject();
     }
+  },
+  (error) => {
+    console.log(error);
+    if (error.response.status == 302) {
+      window.location.href(error.response.headers.Location);
+    }
+    if (error.response.status == 401) {
+      ElMessage.warning("登录过期，请重新登录");
+      router.push("/login");
+    } else {
+      ElMessage.warning("请求错误，请联系管理员");
+    }
+    return Promise.reject();
+  }
 );
 
 export function post(url, params) {
-    // let data = {};
-    // if (url != '/AppCarouselChart/insert' && url != '/AppCarouselChart/update') {
-    //     if (params) {
-    //         const pwdKey = "71B3CC7F6035D9BC1E430D997C88A6BF"
-    //         const sm4 = require('sm-crypto').sm4
-    //         data = { message: sm4.encrypt(JSON.stringify(params), pwdKey, { mode: 'cbc', iv: '15D05CC8DD7045F4BF096B8661300CE1' }) }
-    //     }
-    // } else {
-    //     data = params;
-    // }
-    if (params) {
-        return new Promise((resolve, reject) => {
-            axios
-                // .post(url, data)
-                .post(url, params)
-                .then(response => {
-                    resolve(response.data);
-                })
-                .catch(error => {
-                    reject(error);
-                });
+  // let data = {};
+  // if (url != '/AppCarouselChart/insert' && url != '/AppCarouselChart/update') {
+  //     if (params) {
+  //         const pwdKey = "71B3CC7F6035D9BC1E430D997C88A6BF"
+  //         const sm4 = require('sm-crypto').sm4
+  //         data = { message: sm4.encrypt(JSON.stringify(params), pwdKey, { mode: 'cbc', iv: '15D05CC8DD7045F4BF096B8661300CE1' }) }
+  //     }
+  // } else {
+  //     data = params;
+  // }
+  if (params) {
+    return new Promise((resolve, reject) => {
+      axios
+        // .post(url, data)
+        .post(url, params)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
         });
-    } else {
-        return new Promise((resolve, reject) => {
-            axios
-                .post(url)
-                .then(response => {
-                    resolve(response.data);
-                })
-                .catch(error => {
-                    reject(error);
-                });
+    });
+  } else {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(url)
+        .then((response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          reject(error);
         });
-    }
-
-
+    });
+  }
 }
 
 export function get(url, params) {
-    const pwdKey = "71B3CC7F6035D9BC1E430D997C88A6BF"
-    const sm4 = require('sm-crypto').sm4
-    let data = { message: sm4.encrypt(JSON.stringify(params), pwdKey, { mode: 'cbc', iv: '15D05CC8DD7045F4BF096B8661300CE1' }) }
-    return new Promise((resolve, reject) => {
-        axios
-            .get(url, { data })
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
+  const pwdKey = "71B3CC7F6035D9BC1E430D997C88A6BF";
+  const sm4 = require("sm-crypto").sm4;
+  let data = {
+    message: sm4.encrypt(JSON.stringify(params), pwdKey, {
+      mode: "cbc",
+      iv: "15D05CC8DD7045F4BF096B8661300CE1",
+    }),
+  };
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url, { data })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 export function uploadFile(url, params) {
-    const param = new FormData();
-    Object.keys(params)
-        .filter(key => params[key])
-        .forEach(key => {
-            param.append(key, params[key]);
-        });
-    const config = {
-        headers: { 'Content-Type': 'multipart/form-data' }
-    };
-    return new Promise((resolve, reject) => {
-        axios
-            .post(url, param, config)
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error);
-            });
+  const param = new FormData();
+  Object.keys(params)
+    .filter((key) => params[key])
+    .forEach((key) => {
+      param.append(key, params[key]);
     });
+  const config = {
+    headers: { "Content-Type": "multipart/form-data" },
+  };
+  return new Promise((resolve, reject) => {
+    axios
+      .post(url, param, config)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 export function uploadFileHttp(url, params) {
-    return new Promise((resolve, reject) => {
-        axios
-            .post(url, params, {"responseType":'blob'})
-            .then(response => {
-                resolve(response.data);
-            })
-            .catch(error => {
-                reject(error);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    axios
+      .post(url, params, { responseType: "blob" })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
