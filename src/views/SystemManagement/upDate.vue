c6cc60bbde31314fc3fd8771dc6e457cc079c52a
jackelson <95007281+Jackelson@users.noreply.github.com>
2024-05-07 20:19:15 +0800

    fix: 修复首页图表不展示问题

--- src/views/SystemManagement/upDate.vue
+++ src/views/SystemManagement/upDate.vue
@@ -16,7 +16,7 @@
         <el-input v-model="searchForm.applicant"></el-input>
       </el-form-item>
         <el-form-item label="联系人">
-        <el-input v-model="searchForm.contacts"></el-input>
+        <el-input v-model="searchForm.contacts" placeholder="请填写联系人手机号"></el-input>
       </el-form-item>
       <el-form-item label="时间">
         <el-date-picker
@@ -190,7 +190,7 @@ const total = ref(0);
 const effectData = ref({
     processName:"App_Defect_approval",
     senderId:localStorage.getItem("createById") || '', //这个当前登录人id
-     senderName: localStorage.getItem("userName"),     
+    senderName: localStorage.getItem("userName"),     
 })
 
 // 获取选择框内容
@@ -307,6 +307,28 @@ const close = () => {
 const ruleForm = ref({
   appId: ''
 });
+
+const isMobilePhone = (rule, value, callback) => {
+      if (!value) {
+        return new Error("请填写联系人手机号");
+      } else {
+        const reg = /^1[3|4|5|7|8][0-9]\d{8}$/;
+        const isPhone = reg.test(value);
+        value = Number(value); //转换为数字
+        if (typeof value === "number" && !isNaN(value)) {
+          //判断是否为数字
+          value = value.toString(); //转换成字符串
+          if (value.length < 0 || value.length > 12 || !isPhone) {
+            //判断是否为11位手机号
+            callback(new Error("手机号码格式错误"));
+          } else {
+            callback();
+          }
+        } else {
+          callback(new Error("请输入电话号码"));
+        }
+      }
+};
 const ruleFormRef = ref()
 const rules = ref({
   state: [
@@ -319,7 +341,8 @@ const rules = ref({
     {required: true, message: '请选择状态', trigger: 'blur'}
   ],
   contacts: [
-    {required: true, message: '请填写联系人', trigger: 'blur'}
+    {required: true, message: '请填写联系人手机号' , trigger: 'blur'},
+    { validator: isMobilePhone, trigger: "blur" },
   ],
   applicant: [
     {required: true, message: '请填写申请单位', trigger: 'blur'}
@@ -340,11 +363,13 @@ const submitAdd =async () => {
   if(!v) return;
   loading.value = true;
   let res = {};
+  const data = {...ruleForm.value, createId: localStorage.getItem("createById")};
   if(title.value == '修改') {
-    res = await editUpdate(ruleForm.value).catch(() => loading.value = false);
+    res = await editUpdate(data).catch(() => loading.value = false);
     await upDateEffect({map:{id: ruleForm.value.id}, ...effectData.value }).catch(() => loading.value = false);
   } else {
-    res = await addUpdate(ruleForm.value).catch(() => loading.value = false);
+    res = await addUpdate(data).catch(() => loading.value = false);
+    await upDateEffect({map:{id: ruleForm.value.id}, ...effectData.value }).catch(() => loading.value = false);
   }
   loading.value = false;
   if(res.code == 200) {
